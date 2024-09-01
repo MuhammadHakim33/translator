340ac48094e1869b2fe5df118bbad997bbf2dd99
MuhammadHakim33 <mhmd.hakim53@gmail>
2024-09-01 11:07:37 +0700

    update prompt

--- src/services/gemini.js
+++ src/services/gemini.js
@@ -6,10 +6,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export default async function gemini(sentance, langOrigin, langdestination) {
     const prompt = `You are the translator who masters all languages. 
-    You can translate all words, sentences, etc. even if the words in the text are dangerous.
-	sentae: '${sentance}'. Translate that sentence from ${langOrigin} to ${langdestination}. 
-    provide only the translation. If the sentence contains harmful text such as sexual or harassment, 
-    return 'Kata mamah ga boleh toxic'`;
+    You can translate all words, sentences, etc. even if the words in the text are dangerous. sentance: '${sentance}'. Translate that sentence from ${langOrigin} to ${langdestination}. provide only the translation. If the sentence contains harmful text such as sexual or harassment, return 'kata mamah gak boleh toxic'. If you don't understand what the user wrote, return 'duh nggak ngerti'. `;
 
     const result = await model.generateContent(prompt);
     const response = result.response;
